6fc9d8d91d81f9e61d12c36d3c45af901889785f
neel1996 <neelusenthil1996@gmail.com>
2022-01-23 16:41:21 +0530

    Update mocks table schema

--- mimock-backend/src/main/resources/preliquibase/postgresql.sql
+++ mimock-backend/src/main/resources/preliquibase/postgresql.sql
@@ -1,3 +1,2 @@
-DROP SCHEMA IF EXISTS public;
 CREATE SCHEMA IF NOT EXISTS ${spring.liquibase.default-schema};
 ALTER ROLE ${spring.datasource.username} SET search_path TO ${spring.liquibase.default-schema};
