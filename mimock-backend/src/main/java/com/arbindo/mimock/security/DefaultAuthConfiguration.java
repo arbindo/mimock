2c2e7e819df1796a1dd0778954cf4a2c0048b528
neel1996 <47709856+neel1996@users.noreply.github.com>
2022-03-05 19:52:20 +0530

    [#7] API - Add cors config

--- mimock-backend/src/main/java/com/arbindo/mimock/security/DefaultAuthConfiguration.java
+++ mimock-backend/src/main/java/com/arbindo/mimock/security/DefaultAuthConfiguration.java
@@ -3,6 +3,7 @@ package com.arbindo.mimock.security;
 import com.arbindo.mimock.constants.Role;
 import com.arbindo.mimock.constants.UrlConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,11 +13,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Set;
 
 @EnableWebSecurity
 public class DefaultAuthConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailsService userDetailsService;
+
+    @Value("#{'${app.security.cors-config.allowed-origins}'.split(',')}")
+    private Set<String> corsAllowedOrigins;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -44,7 +53,22 @@ public class DefaultAuthConfiguration extends WebSecurityConfigurerAdapter {
                 )
                 .and().httpBasic().and().authorizeRequests()
                 .antMatchers("/").permitAll()
-                .and().csrf().disable();
+                .and().csrf().disable()
+                .cors().configurationSource(corsConfigurationSource());
+    }
+
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        for (String allowedOrigin : corsAllowedOrigins) {
+            corsConfiguration.addAllowedOrigin(allowedOrigin);
+        }
+        corsConfiguration.addAllowedHeader("Content-type");
+        corsConfiguration.addAllowedHeader("Authorization");
+        source.registerCorsConfiguration(UrlConfig.API_PREFIX + "/**", corsConfiguration);
+
+        return source;
     }
 
     @Override
