2fc1bfe3be62946caae9348a639942124a41f918
neel1996 <47709856+neel1996@users.noreply.github.com>
2022-04-02 22:34:42 +0530

    UI - Update react router setup

--- mimock-ui/src/Routes.js
+++ mimock-ui/src/Routes.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Navigate } from 'react-router-dom';
 import Login from 'components/login';
 import useAxiosInterceptor from 'api/useAxiosInterceptor';
 
 function AppRoutes() {
 	useAxiosInterceptor();
 	return (
-		<Router basename='/mimock-ui/'>
+		<Router basename='/'>
 			<Routes>
-				<Route path='/' element={<Login />} />
+				<Route path='' exact element={<Navigate to='/mimock-ui' />} />
+				<Route path='/mimock-ui' exact element={<Login />} />
 			</Routes>
 		</Router>
 	);
