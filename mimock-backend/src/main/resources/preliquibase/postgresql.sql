4889afcce464d12e02e1fd51ca587a2c7acfaec9
neel1996 <47709856+neel1996@users.noreply.github.com>
2022-07-25 01:09:32 +0530

    API - Remove schema drop query from preliquibase script

--- mimock-backend/src/main/resources/preliquibase/postgresql.sql
+++ mimock-backend/src/main/resources/preliquibase/postgresql.sql
@@ -1,3 +1,2 @@
-DROP SCHEMA IF EXISTS public;
 CREATE SCHEMA IF NOT EXISTS ${spring.datasource.hikari.schema};
 ALTER ROLE ${spring.datasource.username} SET search_path TO ${spring.datasource.hikari.schema};
