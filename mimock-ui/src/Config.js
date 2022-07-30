87e3295a563ced6062ccbe9de0cdd30b6e555573
neel1996 <47709856+neel1996@users.noreply.github.com>
2022-07-30 16:54:46 +0530

    UI - Update URL config for production

--- mimock-ui/src/Config.js
+++ mimock-ui/src/Config.js
@@ -1,7 +1,7 @@
 export const config = {
 	hostName: {
 		development: 'http://localhost:8080',
-		production: 'https://localhost:8080',
+		production: `${window.location.protocol}//${window.location.host}`,
 		ci: 'https://localhost:8080',
 	},
 };
